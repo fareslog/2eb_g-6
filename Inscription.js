04173536b58aa532c8e33fe4d085d40afceca45e
fareslog <faresazzabi986@gmail.com>
2022-12-08 23:24:26 +0100

    first commit

---
+++ Inscription.js
@@ -0,0 +1,56 @@
+var Uv=document.getElementById("username");
+var Ev=document.getElementById("email");
+var Mv=document.getElementById("passe1");
+var Mv1=document.getElementById("passe");
+// let S=document.getElementById("B");
+
+
+ function valide (S) {
+
+    var Uv = false;
+    var Ev = false;
+    let Mv = false;
+    let Mv1 = false;
+     if (Uv === false || Ev === false ) {
+        console.log("hello");
+        S.preventDefault;
+    }
+
+ };
+// function valider(){
+//     nom=document.getElementById('username').Value;
+//     if(/[A-Za-z]/.test(nom)==false){
+//         alert("error");
+//     }
+// }
+
+// console.log("h");
+// document.forms[0].onsubmit =
+
+// || Mv === false || Mv1 === false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Inscription.php
@@ -0,0 +1,4 @@
+<?php
+$s='$localhost',$u='$username',$pass='$pass'
+
+?>
